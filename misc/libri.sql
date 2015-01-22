06cae58bd6b205b15a3e7cd0b1571e51177f5a79
pspgt <gabry.t92@gmail.com>
2015-01-22 17:35:49 +0100

    Aggiunta query inserimento libro

---
+++ misc/libri.sql
@@ -0,0 +1,4 @@
+Insert into Libro 
+values (null, "Il Codice Da Vinci", 3 , 3, 50, 30, "Thriller");
+insert into Libro
+values (null, "Il Signore degli Anelli: La compagnia dell'anello" , 2, 1, 30 , 40 , "Fantasy");
