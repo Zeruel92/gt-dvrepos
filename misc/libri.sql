ac8dd6b38ff39f0a306bbe9116992deb8f0d42aa
pspgt <gabry.t92@gmail.com>
2015-04-07 14:11:08 +0200

    Creato Script SQL per popolare il Database Libri/autori rimossi script parziali

--- misc/libri.sql
+++
@@ -1,4 +0,0 @@
-Insert into Libro 
-values (null, "Il Codice Da Vinci", 3 , 3, 50, 30, "Thriller");
-insert into Libro
-values (null, "Il Signore degli Anelli: La compagnia dell'anello" , 2, 1, 30 , 40 , "Fantasy");
---
+++ misc/populate.sql
@@ -0,0 +1,36 @@
+Insert into casaeditrice
+values(null, "Rusconi Editore", "RUSC8848848");
+Insert into casaeditrice
+values (null, "Salani Editore", "SELANIED7666832");
+Insert into casaeditrice
+values(null, "Mondatori", "MONDA4422328");
+Insert into autore
+values(null, "J.K.","Rowling");
+insert into autore
+values(null, "R.R.", "Tolkien");
+insert into autore 
+values(null, "Dan", "Brown");
+Insert into Libro 
+values (null, "Harry Potter e la pietra filosofale", 1 , 2, 20, 100, "Fantasy");
+Insert into Libro 
+values (null, "Il Codice Da Vinci", 3 , 3, 50, 30, "Thriller");
+insert into Libro
+values (null, "Il Signore degli Anelli: La compagnia dell'anello" , 2, 1, 30 , 40 , "Fantasy");
+insert into autore 
+values(null, "George R.R.", "Martin");
+insert into Libro
+values (null, "Il Gioco Del Trono" , 4, 3, 45 , 0 , "Fantasy");
+insert into Libro
+values (null, "Lo Scontro Dei Re" , 4, 3, 45 , 5 , "Fantasy");
+insert into Libro
+values (null, "Tempesta di Spade" , 4, 3, 45 , 3 , "Fantasy");
+insert into Libro
+values (null, "Il Banchetto dei Corvi" , 4, 3, 45 , 1 , "Fantasy");
+insert into Libro
+values (null, "A Dance with Dragons" , 4, 3, 45 , 2 , "Fantasy");
+insert into autore 
+values(null, "Oscar", "Wilde");
+Insert into casaeditrice
+values(null, "Feltrinelli", "FELTRI4422328");
+insert into Libro
+values (null, "Il Ritratto di Doria Gray" , 5, 4, 15 , 7 , "Romanzo");
