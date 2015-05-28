6684def104d66ed7ff79b11528883aaa809bf653
pspgt <gabry.t92@gmail.com>
2015-05-28 17:08:48 +0200

    Aggiunti controlli sulle giacenze

--- src/it/unisalento/businesslogic/AcquistoManager.java
+++ src/it/unisalento/businesslogic/AcquistoManager.java
@@ -3,6 +3,7 @@ package it.unisalento.businesslogic;
 import it.unisalento.dao.Chart;
 import it.unisalento.dbinterface.DBManager;
 import it.unisalento.model.Utente;
+import javax.swing.JOptionPane;
 
 public class AcquistoManager {
 	
@@ -14,8 +15,17 @@ public class AcquistoManager {
 		java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		String currentTime = sdf.format(dt);
 		for (int i=0;i<c.getDim();i++){
+                   if(c.getItem(i).getGiacenza()>0){
 		String query="Insert into Vendite values (null,"+c.getItem(i).getIdlibro()+","+u.getIdutente()+",\""+currentTime+"\");";
 		d.inserisciNuovo(query);
+                c.getItem(i).setGiacenza(c.getItem(i).getGiacenza()-1);
+                query="UPDATE libro"
+                        + " set giac="+c.getItem(i).getGiacenza()
+                        + " where idlibro="+c.getItem(i).getIdlibro();
+                   }
+                   else {
+                       JOptionPane.showMessageDialog(null,"Libro: "+c.getItem(i).getTitolo()+"non disponibile");
+                   }
 		}
 		c.svuotaCarrello();
 	}
