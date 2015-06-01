0d7901662188041d95a6acad28c45b3e254ab160
pspgt <gabry.t92@gmail.com>
2015-06-01 11:27:59 +0200

    Ottimizzazione simbolo euro

--- src/it/unisalento/Main.java
+++ src/it/unisalento/Main.java
@@ -1,23 +1,14 @@
 package it.unisalento;
 
-import java.net.InetAddress;
+import java.util.Currency;
 
 import it.unisalento.view.Login;
 
 public class Main {
 	public static String EURO;
 	public static void main(String[] args) {
-		String os=System.getProperty("os.name");
-		os=os.toLowerCase();
-		if(os.contains("windows")){
-			EURO="�";
-		}
-		else if (os.contains("osx")){
-			
-		}
-		else{
-			EURO="€";
-		}
+		Currency valuta=Currency.getInstance("EUR");
+		EURO=valuta.getSymbol();
 		Login l=new Login();
 
 	}
