3c93cb96cbcc6ad40b2c47e02fb77a54c76a05f5
pspgt <gabry.t92@gmail.com>
2015-07-14 11:17:23 +0200

    Aggiornati script sql

--- misc/Select libri.sql
+++ misc/Select libri.sql
@@ -1,4 +1,4 @@
-Select l.idLibro, l.titolo, l.genere, l.costo, l.giacenza, a.nome, a.cognome , e.nome as casaed
-from Libro as l, Autore as a, CasaEditrice as e
-where l.idAutore=a.idAutore and l.idCasaEd=e.idCasaEd 
-ORDER BY l.titolo desc
+Select l.idLibro, l.titolo, g.genere, l.costo, l.giacenza, a.nome, a.cognome , e.nome as casaed
+from Libro as l, Autore as a, CasaEditrice as e , Genere as g
+where l.idAutore=a.idAutore and l.idCasaEd=e.idCasaEd and l.idGenere =g.idGenere
+ORDER BY l.titolo
--- misc/populate.sql
+++ misc/populate.sql
@@ -4,33 +4,39 @@ Insert into casaeditrice
 values (null, "Salani Editore", "SELANIED7666832");
 Insert into casaeditrice
 values(null, "Mondatori", "MONDA4422328");
+Insert into casaeditrice
+values(null, "Feltrinelli", "FELTRI4422328");
 Insert into autore
 values(null, "J.K.","Rowling");
 insert into autore
 values(null, "R.R.", "Tolkien");
 insert into autore 
 values(null, "Dan", "Brown");
-Insert into Libro 
-values (null, "Harry Potter e la pietra filosofale", 1 , 2, 20, 100, "Fantasy");
-Insert into Libro 
-values (null, "Il Codice Da Vinci", 3 , 3, 50, 30, "Thriller");
-insert into Libro
-values (null, "Il Signore degli Anelli: La compagnia dell'anello" , 2, 1, 30 , 40 , "Fantasy");
 insert into autore 
 values(null, "George R.R.", "Martin");
-insert into Libro
-values (null, "Il Gioco Del Trono" , 4, 3, 45 , 0 , "Fantasy");
-insert into Libro
-values (null, "Lo Scontro Dei Re" , 4, 3, 45 , 5 , "Fantasy");
-insert into Libro
-values (null, "Tempesta di Spade" , 4, 3, 45 , 3 , "Fantasy");
-insert into Libro
-values (null, "Il Banchetto dei Corvi" , 4, 3, 45 , 1 , "Fantasy");
-insert into Libro
-values (null, "A Dance with Dragons" , 4, 3, 45 , 2 , "Fantasy");
 insert into autore 
 values(null, "Oscar", "Wilde");
-Insert into casaeditrice
-values(null, "Feltrinelli", "FELTRI4422328");
+insert into genere
+values (null, "Fantasy");
+insert into genere
+values (null,"Thriller");
+insert into genere 
+values (null, "Romanzo");
+Insert into Libro 
+values (null, "Harry Potter e la pietra filosofale", 1 , 2, 20, 100, 1);
+Insert into Libro 
+values (null, "Il Codice Da Vinci", 3 , 3, 50, 30, 2);
 insert into Libro
-values (null, "Il Ritratto di Doria Gray" , 5, 4, 15 , 7 , "Romanzo");
+values (null, "Il Signore degli Anelli: La compagnia dell'anello" , 2, 1, 30 , 40 , 1);
+insert into Libro
+values (null, "Il Gioco Del Trono" , 4, 3, 45 , 0 , 1);
+insert into Libro
+values (null, "Lo Scontro Dei Re" , 4, 3, 45 , 5 , 1);
+insert into Libro
+values (null, "Tempesta di Spade" , 4, 3, 45 , 3 ,1);
+insert into Libro
+values (null, "Il Banchetto dei Corvi" , 4, 3, 45 , 1 , 1);
+insert into Libro
+values (null, "A Dance with Dragons" , 4, 3, 45 , 2 , 1);
+insert into Libro
+values (null, "Il Ritratto di Doria Gray" , 5, 4, 15 , 7 , 3);
