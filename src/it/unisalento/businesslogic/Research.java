ab117a29f6c6c851393a40289ad2d00c6c44d547
pistollo <dany_v@live.it>
2015-08-19 16:18:47 +0200

    Continuata classe SForm e Research con Listener

--- src/it/unisalento/businesslogic/Research.java
+++ src/it/unisalento/businesslogic/Research.java
@@ -8,26 +8,33 @@ import it.unisalento.dbinterface.DBManager;
 public class Research {
 	String tipo;
 	String stringa;
-	public Research(String stringa, String tipo){
+	public Research(String tipo, String stringa) throws SQLException{
 		this.tipo=tipo;
 		this.stringa=stringa;
-		if (tipo.equals("Titolo"))
+		
+		
+		if (tipo.equals("titolo"))
 				{
-					String query="SELECT * from Libro as l WHERE titolo=stringa";
+					String query="SELECT * from Libro as l WHERE titolo= stringa";
 					DBManager d=DBManager.getIstance();
 					ResultSet rs=d.eseguiQuery(query);
+					System.out.println(rs.getRow());
 					try {
 						while (rs.next())
 						{
+							
 							System.out.println(rs.getString("titolo"));
+							if(rs.isLast())
+							{
+								break;
+							}
 						}
 					} catch (SQLException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-				}
 		
 		
 	}
-
+}
 }
