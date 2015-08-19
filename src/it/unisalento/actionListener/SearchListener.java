1f90c8678633c9f9ff985425a33012ead8d3ca0c
pspgt <gabry.t92@gmail.com>
2015-08-19 16:59:49 +0200

    Merge

--- src/it/unisalento/actionListener/SearchListener.java
+++ src/it/unisalento/actionListener/SearchListener.java
@@ -5,23 +5,34 @@ import it.unisalento.view.SForm;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 
 public class SearchListener implements ActionListener{
 	private SForm s;
-	private Research r;
+	public String tipo, stringa;
 	public SearchListener(SForm s){
 		this.s=s;
 	}
+	
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
-		if (e.getActionCommand().equals("cerca"))
+		if (e.getActionCommand().equals("search"))
 		{
 			
 			//Research r=new Research();
+			tipo=s.getTipo();
+			stringa=s.getStringa();
+			
+			System.out.println(tipo);
+			System.out.println(stringa);
+				/*try {
+					Research r= new Research(tipo, stringa);
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}*/
 		}
-	}
-	
-
+}
 }
